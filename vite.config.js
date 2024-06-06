6992328e38efae088420ec7e61ed4bb525d767b4
hiroki1117 <scala1117@gmail.com>
2024-06-06 20:39:00 +0900

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO>/',
+  base: process.env.GITHUB_PAGES  // この行を追加
+      ? "reactailwind"            // この行を追加
+      : "./",
 })
